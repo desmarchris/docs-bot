e330d4fe7db1a37a137d2e5b421c01d2455b9082
Chris Desmarais <desmarchris@gmail.com>
2017-07-12 09:44:10 -0400

    Update generateEntityArray.js

--- generateEntityArray.js
+++ generateEntityArray.js
@@ -18,39 +18,4 @@ function generateEntityArray(conversationResponse) {
   });
 }
 
-// response = {
-//   intents: [ { intent: 'entity', confidence: 0.8285990357398987 } ],
-//   entities:
-//    [ { entity: 'intent',
-//        location: [Object],
-//        value: 'what is',
-//        confidence: 1 },
-//      { entity: 'overview',
-//        location: [Object],
-//        value: 'entities',
-//        confidence: 1 },
-//      { entity: 'overview',
-//        location: [Object],
-//        value: 'intents',
-//        confidence: 1 } ],
-//   input: { text: 'what is an entity' },
-//   output: { text: [], nodes_visited: [ 'Entities' ], log_messages: [] },
-//   context:
-//    { conversation_id: 'b54ddfc1-16a3-4c50-951d-027639e6357e',
-//      system:
-//       { dialog_stack: [Object],
-//         dialog_turn_counter: 3,
-//         dialog_request_counter: 3,
-//         _node_output_map: [Object],
-//         branch_exited: true,
-//         branch_exited_reason: 'completed' }
-//    }
-// };
-//
-// var res = generateEntityArray(response);
-// res.then(function(res) {
-//   console.log(res);
-// });
-
-
 module.exports = generateEntityArray;
